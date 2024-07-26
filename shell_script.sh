54234a5fc1564ecc761c9b6273b17609d6330a1b
sanjisdrip <145557623+sanjisdrip@users.noreply.github.com>
2024-07-26 12:14:42 +0300

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -10,10 +10,28 @@ cd Lab1_Dir
 touch testfile.txt
 
 # Writing into the file
-echo Hello World! > testfile.txt
+echo "Hello World!" > testfile.txt
+
+#append text to file
+echo "Another hello world!" >> testfile.txt
 
 # Display the contents of testfile.txt
 cat testfile.txt
 
 # List the files in Lab1_Dir
-ls
+ls -l
+
+#change file permissions
+chmod 644 testfile.txt
+
+#file backup
+cp testfile.txt testfile1.txt
+
+#delete original file
+rm testfile.txt
+
+#move backup file to original file name
+mv testfile1.txt testfile.txt
+
+#display updated file list
+ls -l
